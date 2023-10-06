1d5146a285ea82d9e621ec13a372f1e418b3b08a
reremeireles <renatameirelesbatista@gmail.com>
2023-10-06 15:16:39 -0300

    chore: configure Joi validation

--- backend/src/middleware.ts
+++ backend/src/middleware.ts
@@ -5,7 +5,11 @@ export const validationSchemaMiddleware = (validationSchema: Joi.ObjectSchema) =
   return async function (req: Request, res: Response, next: NextFunction) {
     try {
       const data = req.body;
-      await validationSchema.validateAsync(data);
+      await validationSchema.validateAsync(data, {
+        abortEarly: false,
+        allowUnknown: false,
+        convert: false
+      });
       return next();
     } catch (error: any) {
       return res.status(400).json({
