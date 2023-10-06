1ce56e89a51291ab3b1732738f2fe71d00d83960
Marcos Santana <brmarcossantanabr@gmail.com>
2023-10-05 22:06:38 -0300

    feat: add products crud

--- backend/src/products/products.routes.ts
+++ backend/src/products/products.routes.ts
@@ -1,34 +1,53 @@
 import { Request, Response, Router } from 'express'
+import { database } from '../db';
+import { Product } from './product.model';
 
 export const productsRouter = Router();
 
+const productRepository = database.getRepository(Product)
+
 productsRouter.post('/products', (req: Request, res: Response) => {
+  const data = req.body;
+  const newProduct = {
+    ...data,
+    created_at: new Date(),
+    updated_at: new Date(),
+  };
+  productRepository.insert(newProduct)
   res.sendStatus(201);
 });
 
-productsRouter.put('/products', (req: Request, res: Response) => {
+productsRouter.put('/products/:id', async (req: Request, res: Response) => {
+
+  const productId = Number(req.params.id);
+  const dataToUpdate = req.body;
+
+  await productRepository.update(productId, dataToUpdate)
+
   res.sendStatus(204);
 });
 
-productsRouter.get('/products/:id', (req: Request, res: Response) => {
-  res.status(200).json({
-    id: req.params.id,
-    title: 'Product title',
-    description: 'Product description',
-    price: 1000,
-    image: 'https://via.placeholder.com/200x200',
-  });
+
+productsRouter.delete('/products/:id', async (req: Request, res: Response) => {
+  const productId = Number(req.params.id);
+  await productRepository.delete(productId)
+
+  res.status(204)
 });
 
-productsRouter.get('/products', (req: Request, res: Response) => {
+productsRouter.get('/products/:id', async (req: Request, res: Response) => {
+  const product = await productRepository.findOne({
+    where: {
+      id: Number(req.params.id)
+    }
+  })
+  res.status(200).json(product);
+});
+
+productsRouter.get('/products', async (req: Request, res: Response) => {
+  const products = await productRepository.find();
   res.status(200).json({
-    data: [{
-      id: 1,
-      title: 'Product title',
-      description: 'Product description',
-      price: 1000,
-      image: 'https://via.placeholder.com/200x200',
-    }],
+    data: products,
     page: 1,
     nextPage: 2,
     previousPage: null,
